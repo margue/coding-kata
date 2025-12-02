286c08ac27ee76a9457a0a366e391360f0b4d0cc
Martin Günther <tools@martinguenther-consulting.de>
2025-12-02 15:20:45 +0100

    :memo: add example test

--- src/test/java/de/martinguenther/kata/KataTest.java
+++ src/test/java/de/martinguenther/kata/KataTest.java
@@ -1,4 +1,13 @@
 package de.martinguenther.kata;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class KataTest {
+
+    @Test
+    void testSomething() {
+        assertThat(true).isTrue();
+    }
 }
